7eb01ec55242959710e437c7148f6a2a372499e3
Mohammad Bilal <mohammadbilal3100@gmail.com>
2024-09-30 10:00:03 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,178 @@
+export const Bio = {
+  name: "Mohammad Bilal",
+  roles: [
+    "Fresher",
+    "Job Seeker",
+    // "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I wish to work with well reputed company which will help me to explore and achieve expertise through opportunities and learning, which help me to enhance my skills along with ensuring company's success.",
+  github: "https://github.com/bilal-github-cpu",
+  resume:
+    "https://drive.google.com/file/d/1WPXRSxZPuZSxbml3aysc9bzSjTagjYEz/view",
+  linkedin: "linkedin.com/in/bilal-1234-mohammad",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/009/032/613/non_2x/git-logo-git-letter-git-letter-logo-design-initials-git-logo-linked-with-circle-and-uppercase-monogram-logo-git-typography-for-technology-business-and-real-estate-brand-vector.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://vectorseek.com/wp-content/uploads/2023/05/Postman-Logo-Vector-730x730.jpg",
+      },
+      {
+        name: "Eclips",
+        image:
+          "https://th.bing.com/th/id/R.99f307973d55dadbdd26425b722e5430?rik=13HLP0n%2f5OVc0w&riu=http%3a%2f%2feclipse.org%2forg%2fartwork%2fimages%2feclipse_ide_logo.png&ehk=IF9%2bgPNa68PTjpSC3ngwZBPnRE9T%2bQDHjFhspI54zCs%3d&risl=&pid=ImgRaw&r=0",
+      },
+      {
+        name: "STS(Spring Tool Suit)",
+        image:
+          "https://th.bing.com/th/id/OIP.AI0PFPhmPliXAWLxOwqZCQHaHw?rs=1&pid=ImgDetMain",
+      },
+
+    ],
+  },
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Core Java",
+        image:"https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png",
+      },
+      {
+        name: "Advanced Java",
+        image:
+          "https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png",
+      },
+
+      {
+        name: "spring",
+        image:
+          "https://th.bing.com/th/id/OIP.AI0PFPhmPliXAWLxOwqZCQHaHw?rs=1&pid=ImgDetMain",
+         
+      },
+      {
+        name: "SpringBoot&Microservice",
+        image:
+          "https://vectorseek.com/wp-content/uploads/2023/05/Postman-Logo-Vector-730x730.jpg",
+      },
+
+    ],
+  },
+
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Employee Management system",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "This is desktop application which is with the help of SpringBoot,HTML,CSS and Bootstrap.I used Mysql database also for stroing of employees data",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "SpringBoot",
+      "HTML",
+      "CSS",
+      "BootStrap",
+      "Mysql",
+    ],
+    category: "web app",
+  
+
+  },
+
+  {
+    id: 1,
+    title: "RestApi",
+    date: "Jun 2024",
+    description:
+      "I made RestApi with the help of using SpringBoot and In database i used Mysql database.",
+    image:
+      "https://img.icons8.com/ink/48/api.png",
+    tags: ["SpringBoot", "API", "Mysql",],
+    
+   
+
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.motachashma.com/images/exams/jamia-hamdard-university-admission.jpg",
+    school: "Jamia Hamdard University",
+    date: "Oct 2021 - Sep 2024",
+    desc: "I am currently graduate in Bachelor's of Computer Application at Jamia Hmadard University,Delhi. I have completed 6 semesters and have a CGPA of 7.2",
+    degree: "Bachelor of Computer Application - BCA, Computer Science and Application",
+  },
+  {
+    id: 1,
+    img: "https://indianmasterminds.com/wp-content/uploads/2022/01/delhi-government-logo-640x419.jpg",
+    school: "Rajkiya Sarvodaya Vidyalaya",
+    date: "Apr 2019 - Apr 2021",
+    desc: "I completed my class 12 high school education at Rajkiya Sarvodaya Vidyalaya, where I studied Commerce with Maths.",
+
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/OIP.DgEn2OlXoZx-XVjU3p87_AHaGg?rs=1&pid=ImgDetMain",
+    school: "Vivekanand public school",
+    date: "Apr 2017 - Apr 2019",
+    desc: "I completed my class 10 education at Vivekanand Public School, where I studied Science with maths.",
+
+  },
+];
